1dbd5ababb65c13e898487257d502ceb81fe0c70
GustavoGentofanti <gustavogentofanti.pro@gmail.com>
2022-05-20 18:48:48 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,6 +27,14 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println();
+
+        System.out.println("\n=== TEST 2: seller findAll =====");
+        list = sellerDao.findAll();
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
         System.out.println(seller);
     }
 }
